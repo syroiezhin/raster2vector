baf415f545cea497ac34a3deb980bf22b1b49351
Valerij <v.syroiezhin@gmail.com>
2022-07-26 02:48:40 +0300

    Create raster2vector.py

--- raster2vector.py
+++ raster2vector.py
@@ -1,21 +1,25 @@
-import os
-import base64
-import struct
-import imghdr
- 
-def get_image_size(fname):
+from os import listdir, getcwd, mkdir, path # firstWay + secondWay
+
+from base64 import b64encode # firstWay
+from struct import unpack #firstWay
+from imghdr import what #firstWay
+
+from pathlib import Path # secondWay
+from svgtrace import trace # secondWay
+
+def get_image_size_for_first_way(fname):
     with open(fname, 'rb') as fhandle:
         head = fhandle.read(24)
         if len(head) != 24:
             return
-        if imghdr.what(fname) == 'png':
-            check = struct.unpack('>i', head[4:8])[0]
+        if what(fname) == 'png':
+            check = unpack('>i', head[4:8])[0]
             if check != 0x0d0a1a0a:
                 return
-            width, height = struct.unpack('>ii', head[16:24])
-        elif imghdr.what(fname) == 'gif':
-            width, height = struct.unpack('<HH', head[6:10])
-        elif imghdr.what(fname) == 'jpeg':
+            width, height = unpack('>ii', head[16:24])
+        elif what(fname) == 'gif':
+            width, height = unpack('<HH', head[6:10])
+        elif what(fname) == 'jpeg':
             try:
                 fhandle.seek(0)
                 size = 2
@@ -26,16 +30,16 @@ def get_image_size(fname):
                     while ord(byte) == 0xff:
                         byte = fhandle.read(1)
                     ftype = ord(byte)
-                    size = struct.unpack('>H', fhandle.read(2))[0] - 2
+                    size = unpack('>H', fhandle.read(2))[0] - 2
                 fhandle.seek(1, 1)
-                height, width = struct.unpack('>HH', fhandle.read(4))
+                height, width = unpack('>HH', fhandle.read(4))
             except Exception:
                 return
         else:
             return
         return width, height
 
-def svg():
+def firstWay_raster2vector():
     
     start_svg_tag = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
     <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN"
@@ -44,20 +48,40 @@ def svg():
     """
     end_svg_tag = """</svg>"""
     
-    for image in os.listdir(os.getcwd()+ "/"): # Image must be in the folder with the program
+    for image in listdir(path2png):
         if image.endswith(".jpg") or image.endswith(".png") or image.endswith(".gif"):
-            width, height = get_image_size(image)
-            img_file = open(image, 'rb')
-            base64data = base64.b64encode(img_file.read())
+            width, height = get_image_size_for_first_way(path2png + image)
+            img_file = open(path2png + image, 'rb')
+            base64data = b64encode(img_file.read())
             base64String = f'<image xlink:href="data:image/png;base64,{base64data.decode("utf-8")}" width="{width}" height="{height}" x="0" y="0" />'
             svg_size = f'width="{width}px" height="{height}px" viewBox="0 0 {width} {height}">'
             
-            if os.path.exists('result') == False: os.mkdir('result') # create a folder "result"
-            
-            svg = open("result/"+os.path.splitext(image)[0]+".svg",'w') # write the result to a folder
+            svg = open(path2svg + path.splitext(image)[0] + "[1].svg",'w')
             svg.write( start_svg_tag + svg_size + base64String + end_svg_tag)
-            print ('Converted '+ image + ' to ' + os.path.splitext(image)[0]+".svg")
+            print ('Converted '+ image + ' to ' + path.splitext(image)[0]+".svg")
+
+
+
+
+
+def secondWay_raster2vector(): 
+    for image in listdir(path2png):
+        if image.endswith(".jpg") or image.endswith(".png") or image.endswith(".gif"):
+            Path(path2svg + image[0] + "[2].svg").write_text(trace(path2png + image), encoding="utf-8")
+
+
+
 
 
 if __name__== '__main__':
-    svg()
+    
+    # if you do not have a folder, 
+    # the program will create it, 
+    # and you will have to put your images inside the folder
+    if path.exists('image') == False: mkdir('image')
+    
+    path2png = getcwd() + "/image/"
+    path2svg = getcwd() + "/svg/"
+    if path.exists('svg') == False: mkdir('svg') # create a folder "svg"
+    firstWay_raster2vector()
+    secondWay_raster2vector()
